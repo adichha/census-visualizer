e2e8758e494455e332c05b6584828703b9f4c858
Shehryar Assad <xshehryar@gmail.com>
2020-06-12 01:19:48 -0400

    Add map

--- client/src/pages/dashboard/Map.js
+++ client/src/pages/dashboard/Map.js
@@ -5,21 +5,25 @@ export class Map extends Component {
 
   state = {
     viewport: {
-      width: 400,
-      height: 400,
       latitude: 37.7577,
       longitude: -122.4376,
       zoom: 8
     }
   };
 
+
+  onViewportChange = viewport => {
+    const { width, height, ...etc } = viewport
+    this.setState({ viewport: etc })
+  }
   render() {
     return (
       <div style={{ height: '100%', position: 'relative' }}>
-
         <ReactMapGL
+          width='100%'
+          height='100%'
           {...this.state.viewport}
-          onViewportChange={(viewport) => this.setState({ viewport })}
+          onViewportChange={viewport => this.onViewportChange(viewport)}
           mapboxApiAccessToken='pk.eyJ1IjoidHBpbnRvNyIsImEiOiJja2JicWYwMzkwM3NnMnNtZnZkbXU5dGhkIn0.NdzHwoMYvZ-fSTIA9xXXfw'
         />
       </div>
