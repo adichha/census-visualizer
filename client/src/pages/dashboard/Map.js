459996262d5bfb90d3ad0881efed407c58bdcf1d
tpinto7 <tjpinto@uwaterloo.ca>
2020-06-14 15:46:23 -0400

    Added heatmap layer

--- client/src/pages/dashboard/Map.js
+++ client/src/pages/dashboard/Map.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Source, Layer} from 'react-map-gl';
+import {heatmapLayer} from './map-style';
+import {json as requestJson} from 'd3-request';
 
 export class Map extends Component {
 
@@ -8,15 +10,29 @@ export class Map extends Component {
       latitude: 37.7577,
       longitude: -122.4376,
       zoom: 8
-    }
+    },
+    data: null
   };
 
+  componentDidMount(){
+    requestJson(
+      'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+      (error, response) => {
+        if (!error) {
+          this.setState({
+            data: response
+          });
+        }
+      }
+    );
+  }
 
   onViewportChange = viewport => {
     const { width, height, ...etc } = viewport
     this.setState({ viewport: etc })
   }
   render() {
+    const {data} = this.state
     return (
       <div style={{ height: '100%', position: 'relative' }}>
         <ReactMapGL
@@ -25,7 +41,11 @@ export class Map extends Component {
           {...this.state.viewport}
           onViewportChange={viewport => this.onViewportChange(viewport)}
           mapboxApiAccessToken='pk.eyJ1IjoidHBpbnRvNyIsImEiOiJja2JicWYwMzkwM3NnMnNtZnZkbXU5dGhkIn0.NdzHwoMYvZ-fSTIA9xXXfw'
-        />
+        >
+        {data && (<Source type="geojson" data={data}>
+          <Layer {...heatmapLayer} />
+        </Source>)}
+        </ReactMapGL> 
       </div>
     );
   }
