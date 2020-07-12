44ed9471a0763fd6866988a631b6d3d14e9777ab
adichha <a22chhab@uwaterloo.ca>
2020-07-12 19:41:21 -0400

    README and python script edit

--- README.md
+++ README.md
@@ -6,5 +6,17 @@ We are running MySQL through Amazon RDS running in an EC2 instance. To set it up
 
 We plan to export the publicly available census data on the Canadian and American government websites as .csv files. We will sanitize the input, wrangle it to conform to our schema, and then insert it into the database.
 
+The data for the income table can be downloaded from this link by clicking download entire table CSV file:
+
+https://www12.statcan.gc.ca/census-recensement/2016/dp-pd/dt-td/Rp-eng.cfm?TABID=4&LANG=E&A=R&APATH=3&DETAIL=0&DIM=0&FL=A&FREE=0&GC=01&GL=-1&GID=1341679&GK=1&GRP=1&O=D&PID=110587&PRID=10&PTYPE=109445&S=0&SHOWALL=0&SUB=0&Temporal=2017&THEME=131&VID=0&VNAMEE=&VNAMEF=&D1=0&D2=0&D3=0&D4=0&D5=0&D6=0
+
+In order to sanitize the data, run the function sanitize_income in /db/csv_editor.py. Make sure that the csv downloaded from the website is in the same directory as the python script. The outputted file (income_out.py) will contain the data in a format which can be uploaded into the table.
+
+The data for the employment table can be downloaded from this link by clicking download entire table CSV file:
+
+https://www12.statcan.gc.ca/census-recensement/2016/dp-pd/dt-td/Rp-eng.cfm?TABID=4&LANG=E&A=R&APATH=3&DETAIL=0&DIM=0&FL=A&FREE=0&GC=01&GL=-1&GID=1257309&GK=1&GRP=1&O=D&PID=111848&PRID=10&PTYPE=109445&S=0&SHOWALL=0&SUB=0&Temporal=2017&THEME=124&VID=0&VNAMEE=&VNAMEF=&D1=0&D2=0&D3=0&D4=0&D5=0&D6=0
+
+In order to sanitize the data, run the function sanitize_employment in /db/csv_editor.py. Make sure that the csv downloaded from the website is in the same directory as the python script. The outputted file (employment_out.py) will contain the data in a format which can be uploaded into the table.
+
 In order to create the sample database execute the create table script in the db folder. In order to populate 
-the sample database run test-sample.sql in the queries folder. 
+the sample database run test-sample.sql in the queries folder.
--- db/csv_editor.py
+++ db/csv_editor.py
@@ -6,43 +6,47 @@ all_sex = {'Total - Sex': 1, 'Male': 2, 'Female': 3}
 count = 0
 cma = {}
 
-with open('labour.csv') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        if len(row[1]) == 7 and row[11] in valid_age and row[9] == '1' and row[18] == '1':
-            print(row)
-            valid_entries.append([count, row[12], row[15], 2016,'can',row[1], row[22], 1])
-            count+=1
-            valid_entries.append([count, row[12], row[15], 2016,'can',row[1], row[23], 2])
-            count+=1
 
-with open('labour_out.csv', 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=["id", "age", "sex", "year", "region","geocode", "value", "meta"])
-    writer.writeheader()
-    for i in valid_entries:
-        writer.writerow({'id': i[0], 'age': i[1], 'sex': i[2], 'year': i[3] , 'region': i[4],'geocode': i[5], 'value': i[6], 'meta': i[7]})
+def sanitize_income():
+    count = 0
+    with open('income.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
 
-# with open('income.csv') as csv_file:
-#     reader = csv.reader(csv_file)
-#     for row in reader:
-#         if len(row[0]) == 3 and row[0] not in cma:
-#             cma['9999' + row[0]] = 1
-#
-#         if (len(row[0]) == 3) and row[1] in valid_age:
-#             valid_entries.append([count, valid_age[row[1]], all_sex[row[2]], 2016,'ca','9999' + row[0], row[5], row[4]])
-#             count+=1
+            if (len(row[0]) == 3) and row[1] in valid_age:
+                valid_entries.append([count, valid_age[row[1]], all_sex[row[2]], 2016,'ca','9999' + row[0], row[5], row[4]])
+                count+=1
+
+    with open('income_out.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=["id", "age", "sex", "year", "region","geocode", "value", "meta"])
+        writer.writeheader()
+        for i in valid_entries:
+            writer.writerow({'id': i[0], 'age': i[1], 'sex': i[2], 'year': i[3] , 'region': i[4],'geocode': i[5], 'value': i[6], 'meta': i[7]})
+
+def sanitize_employment():
+    count = 0
+    with open('employment.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            if row[1] == '01' and row[9] == '1' and row[12] == '1':
+                print(row)
+            if len(row[1]) == 7 and row[11] in valid_age and row[9] == '1' and row[18] == '1':
+                valid_entries.append([count, row[12], row[15], 2016,'can',row[1], row[22], 1])
+                count+=1
+                valid_entries.append([count, row[12], row[15], 2016,'can',row[1], row[23], 2])
+                count+=1
+
+    with open('employment_out.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=["id", "age", "sex", "year", "region","geocode", "value", "meta"])
+        writer.writeheader()
+        for i in valid_entries:
+            writer.writerow({'id': i[0], 'age': i[1], 'sex': i[2], 'year': i[3] , 'region': i[4],'geocode': i[5], 'value': i[6], 'meta': i[7]})
 
 # with open('CMAs.csv') as csv_file:
 #     reader = csv.reader(csv_file)
 #     for row in reader:
 #         if row[0] not in cmaCheck:
 #             cmaCheck[row[0]] = 1
-
-# with open('income_out.csv', 'w', newline='') as csvfile:
-#     writer = csv.DictWriter(csvfile, fieldnames=["id", "age", "sex", "year", "region","geocode", "value", "meta"])
-#     writer.writeheader()
-#     for i in valid_entries:
-#         writer.writerow({'id': i[0], 'age': i[1], 'sex': i[2], 'year': i[3] , 'region': i[4],'geocode': i[5], 'value': i[6], 'meta': i[7]})
 
 # for i in cma.keys():
 #     if i not in cmaCheck:
