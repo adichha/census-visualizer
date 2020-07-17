7520a60c76fcfb9f932e87b9dac73e8bc2956122
MannDP <patelmann1999@gmail.com>
2020-07-17 14:21:26 -0600

    update script to sanitize data + update readme

--- db/csv_editor.py
+++ db/csv_editor.py
@@ -42,6 +42,105 @@ def sanitize_employment():
         for i in valid_entries:
             writer.writerow({'id': i[0], 'age': i[1], 'sex': i[2], 'year': i[3] , 'region': i[4],'geocode': i[5], 'value': i[6], 'meta': i[7]})
 
+def sanitize_education():
+    count = 0
+    with open('education.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            if len(str(row[1])) >= 7 and (str(row[21]) != '0' or str(row[22]) != '0'):
+                if row[14] in 'Total - Age':
+                    valid_entries.append(
+                        [count, valid_age['Total - Age'], 2, 2016, 'can', row[1], row[21], row[18]])
+                    count += 1
+                    valid_entries.append(
+                        [count, valid_age['Total - Age'], 3, 2016, 'can', row[1], row[22], row[18]])
+                    count += 1
+                    valid_entries.append(
+                        [count, valid_age['Total - Age'], 1, 2016, 'can', row[1], row[20], row[18]])
+                    count += 1
+                else:
+                    if row[14] in valid_age:
+                        # male data
+                        valid_entries.append(
+                            [count, valid_age[row[14]], 2, 2016, 'can', row[1], row[21], row[18]])
+                        count += 1
+                        # female data
+                        valid_entries.append(
+                            [count, valid_age[row[14]], 3, 2016, 'can', row[1], row[22], row[18]])
+                        count += 1
+                        
+                        #sum the data for each gender
+                        valid_entries.append(
+                            [count, valid_age[row[14]], 1, 2016, 'can', row[1], row[20], row[18]])
+                        count += 1
+        
+        with open('education_out.csv', 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=[
+                                    "id", "age", "sex", "year", "region", "geocode", "value", "meta"])
+            writer.writeheader()
+            for i in valid_entries:
+                writer.writerow({'id': i[0], 'age': i[1], 'sex': i[2], 'year': i[3], 'region': i[4], 'geocode': i[5], 'value': i[6], 'meta': i[7]})
+  
+
+def santize_population():
+    count = 0
+
+    def age_to_range_key(age):
+        key = ''
+        if age >= 15 and age <= 24:
+            key = '15 to 24 years'
+        elif age >= 25 and age <= 34:
+            key = '25 to 34 years'
+        elif age >= 35 and age <= 44:
+            key = '35 to 44 years'
+        elif age >= 45 and age <= 54:
+            key = '45 to 54 years'
+        elif age >= 55 and age <= 64:
+            key = '55 to 64 years'
+        else:
+            key = '65 years and over'
+        return key
+
+    with open('population.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            # 7 digit geocode
+            if len(str(row[1])) == 7:
+                age = row[11]
+                try:
+                    age = int(age)
+                    key = age_to_range_key(age)
+                    if key:
+                        valid_entries.append([count, valid_age[key], 1, 2016, 'can', row[1], row[14], 0])
+                        count += 1
+                        
+                        #only male
+                        valid_entries.append([count, valid_age[key], 2, 2016, 'can', row[1], row[15], 0])
+                        count += 1
+                        
+                        #only female
+                        valid_entries.append([count, valid_age[key], 3, 2016, 'can', row[1], row[16], 0])
+                        count += 1
+                except:
+                    if age in 'Total - Age':
+                        valid_entries.append([count, 1, 1, 2016, 'can', row[1], row[14], 0])
+                        count += 1
+                        
+                        #only male
+                        valid_entries.append([count, 1, 2, 2016, 'can', row[1], row[15], 0])
+                        count += 1
+                        
+                        #only female
+                        valid_entries.append([count, 1, 3, 2016, 'can', row[1], row[16], 0])
+                        count += 1
+            
+
+    with open('population_out.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=["id", "age", "sex", "year", "region","geocode", "value", "meta"])
+        writer.writeheader()
+        for i in valid_entries:
+            writer.writerow({'id': i[0], 'age': i[1], 'sex': i[2], 'year': i[3] , 'region': i[4],'geocode': i[5], 'value': i[6], 'meta': i[7]})
+
 # with open('CMAs.csv') as csv_file:
 #     reader = csv.reader(csv_file)
 #     for row in reader:
