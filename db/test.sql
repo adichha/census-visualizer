8141c6e66fd5a6d989789922619544559c6b98bd
Shahbaz Momi <shahbaz.man3@gmail.com>
2020-08-09 22:46:52 -0400

    updated SQLs

--- db/test.sql
+++ db/test.sql
@@ -1,5 +1,21 @@
-SELECT lon, lat, SUM(value)
-FROM employment
-NATURAL JOIN geocode_lut
-WHERE age IN (2, 5) AND sex = 1 AND meta IN (2)
-GROUP BY geocode, lon, lat;
+SELECT lon, lat, SUM(e.value) / SUM(e2.value)
+FROM employment e
+NATURAL JOIN geocode_lut g, employment e2
+WHERE e.age IN (2, 5) AND e.sex = 1 AND e.meta IN (1, 2) AND e2.age IN (2, 5) AND e2.sex = 1 AND e2.meta IN (1, 2) AND e.geocode = g.geocode AND e2.geocode = g.geocode
+GROUP BY g.geocode, g.lon, g.lat;
+
+SELECT lon, lat, e.value, SUM(e2.value)
+FROM education e
+INNER JOIN geocode_lut g ON e.geocode = g.geocode
+INNER JOIN education e2 ON e2.geocode = g.geocode AND e.sex = e2.sex AND e.age = e2.age
+WHERE e.sex = 1 AND e.age IN (1) AND e.meta = (2)
+    AND e2.meta = 1
+GROUP BY g.geocode, g.lon, g.lat, e.value;
+
+
+SELECT e.value / SUM(e2.value), g.lon, g.lat
+FROM employment e
+INNER JOIN employment e2 ON e.geocode = e2.geocode AND e.sex = e2.sex AND e.age = e2.age
+INNER JOIN geocode_lut g ON g.geocode = e.geocode
+WHERE e.sex = 1 AND e.age = 1 AND e.meta = 2
+GROUP BY g.lon, g.lat, e.value;
--- queries/test_production.sql
+++ queries/test_production.sql
@@ -2,36 +2,54 @@
 -- Filtering by age, sex, and meta parameters
 SELECT lon, lat, SUM(value)
 FROM education
-NATURAL JOIN geocode_lut
-WHERE meta IN (2, 3) AND age IN (4, 5) AND sex = (2)
+         NATURAL JOIN geocode_lut
+WHERE meta IN (2, 3)
+  AND age IN (4, 5)
+  AND sex = (2)
 GROUP BY geocode, lon, lat
 LIMIT 10;
 
 SELECT lon, lat, SUM(value)
 FROM population
-NATURAL JOIN geocode_lut
-WHERE age IN (4, 5) AND sex = (2)
+         NATURAL JOIN geocode_lut
+WHERE age IN (4, 5)
+  AND sex = (2)
 GROUP BY geocode, lon, lat
 LIMIT 10;
 
 SELECT lon, lat, SUM(value)
 FROM employment
-NATURAL JOIN geocode_lut
-WHERE meta IN (2, 3) AND age IN (4, 5) AND sex = (2)
+         NATURAL JOIN geocode_lut
+WHERE meta IN (2, 3)
+  AND age IN (4, 5)
+  AND sex = (2)
 GROUP BY geocode, lon, lat
 LIMIT 10;
 
 SELECT lon, lat, SUM(value)
 FROM individual_incomes
-NATURAL JOIN geocode_lut
-WHERE meta IN (2, 3) AND age IN (4, 5) AND sex = (2)
+ NATURAL JOIN geocode_lut
+WHERE meta IN (2, 3)
+  AND age IN (4, 5)
+  AND sex = (2)
 GROUP BY geocode, lon, lat
 LIMIT 10;
+
+SELECT SUM(e.value) / SUM(p.value) as count, g.lat, g.lon
+FROM employment e
+NATURAL JOIN geocode_lut g,
+     population p
+WHERE e.meta IN :metas
+  AND e.age IN :ages
+  AND e.sex = :sex
+  AND e.geocode = g.geocode
+  AND p.geocode = g.geocode
+GROUP BY g.geocode, g.lon, g.lat;
 
 -- Get a user profile
 SELECT *
 FROM user_profiles
-WHERE username='johndoe@gmail.com6';
+WHERE username = 'johndoe@gmail.com6';
 
 -- Create a user profile
 INSERT INTO user_profiles (username, first_name, last_name, password_hash, salt, num_queries)
@@ -48,19 +66,24 @@ SELECT *
 FROM user_profiles
 WHERE visibility = 'public'
     AND username LIKE '%johnny%'
-    OR first_name LIKE '%john%'
-    OR last_name LIKE 'doe'
+   OR first_name LIKE '%john%'
+   OR last_name LIKE 'doe'
 ORDER BY username DESC
 LIMIT 25;
 
 -- Get the saved queries for the user
-SELECT query FROM saved_queries
-WHERE uid in(
-    SELECT uid FROM user_profiles
+SELECT query
+FROM saved_queries
+WHERE uid in (
+    SELECT uid
+    FROM user_profiles
     WHERE username = 'johndoe@gmail.com6'
-    )
+)
 ;
 
 -- Create a saved query
-INSERT INTO saved_queries (uid, last_updated, query) 
-VALUES (1, NOW(), '{"key1": "value1"}');
+INSERT INTO saved_queries (uid, last_updated, query)
+VALUES (1, NOW(), '{
+  "key1": "value1"
+}');
+
